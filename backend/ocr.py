a7796bcc01a686da7c5eab93fc3b81450192506e
vedantware06 <vedantware6@gmail.com>
2026-08-03 21:47:13 +0530

    Remove Windows Tesseract path

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,17 +1,10 @@
 import fitz
-import pytesseract
-from PIL import Image
 import os
-
-# Tesseract path
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
 
 def extract_text(file_path):
 
     ext = os.path.splitext(file_path)[1].lower()
 
-    # PDF
     if ext == ".pdf":
         doc = fitz.open(file_path)
 
@@ -23,12 +16,4 @@ def extract_text(file_path):
         doc.close()
         return text
 
-    # Image
-    elif ext in [".jpg", ".jpeg", ".png"]:
-
-        image = Image.open(file_path)
-        text = pytesseract.image_to_string(image)
-        return text
-
-    else:
-        return ""
+    return ""
