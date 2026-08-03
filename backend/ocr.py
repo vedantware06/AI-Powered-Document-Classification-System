1d028ae03a57a4cc86b2ea41a9290af881a66048
vedantware06 <vedantware6@gmail.com>
2026-08-03 21:51:04 +0530

    Updated OCR for deployment

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,19 +1,44 @@
 import fitz
 import os
+from PIL import Image
+import pytesseract
+
 
 def extract_text(file_path):
 
     ext = os.path.splitext(file_path)[1].lower()
 
+    # PDF
     if ext == ".pdf":
-        doc = fitz.open(file_path)
+        try:
+            doc = fitz.open(file_path)
 
-        text = ""
+            text = ""
 
-        for page in doc:
-            text += page.get_text()
+            for page in doc:
+                text += page.get_text()
 
-        doc.close()
-        return text
+            doc.close()
 
-    return ""
+            return text.strip()
+
+        except Exception as e:
+            print("PDF Error:", e)
+            return ""
+
+    # Image
+    elif ext in [".jpg", ".jpeg", ".png"]:
+        try:
+            image = Image.open(file_path)
+
+            text = pytesseract.image_to_string(image)
+
+            return text.strip()
+
+        except Exception as e:
+            print("OCR Error:", e)
+            return ""
+
+    # Unsupported File
+    else:
+        return ""
